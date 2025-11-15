1a4a81ed6dd8e1f8dad23fe0df21e58289b2fece
Vsevolod Ryzhov <v.ryzhov@plein.com>
2025-11-15 10:33:40 +0300

    iter11: double import fix

--- internal/repository/postgresRepository.go
+++ internal/repository/postgresRepository.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/golang-migrate/migrate/v4/source/github"
 	_ "github.com/jackc/pgx/v5/stdlib"
