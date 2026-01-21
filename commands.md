2d16debd453cc716311fe930891b77ffce08659a
Vsevolod Ryzhov <v.ryzhov@plein.com>
2026-01-21 19:52:15 +0300

    iter17: project profiling and optimization

--- commands.md
+++ commands.md
@@ -16,6 +16,7 @@
 14. shortenertest_v2-darwin-arm64 -test.v -test.run=^TestIteration14$ -binary-path=cmd/shortener/shortener -database-dsn="host=localhost user=postgres_user password=postgres_password dbname=postgres_db sslmode=disable"
 15. shortenertest_v2-darwin-arm64 -test.v -test.run=^TestIteration15$ -binary-path=cmd/shortener/shortener -database-dsn="host=localhost user=postgres_user password=postgres_password dbname=postgres_db sslmode=disable"
 16. shortenertest_v2-darwin-arm64 -test.v -test.run=^TestIteration16$ -source-path=.
+17. shortenertest_v2-darwin-arm64 -test.v -test.run=^TestIteration17$ -source-path=.
 
 ## Misc
 - go build -o cmd/shortener/shortener cmd/shortener/*.go
--- internal/handler/router_test.go
+++ internal/handler/router_test.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -69,4 +71,47 @@ func TestHandlerGETNotFound(t *testing.T) {
 	if recorder.Code != http.StatusNotFound {
 		t.Errorf("handler returned wrong status code: got %v want %v", recorder.Code, http.StatusNotFound)
 	}
+}
+func BenchmarkMultiplePostSuccess(b *testing.B) {
+	os.Remove(config.Options.StorageFilePath)
+	for i := 0; i < b.N; i++ {
+		requestBody := "https://" + generateRandomString(32) + ".ru"
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(requestBody))
+
+		recorder := httptest.NewRecorder()
+
+		handleCreateLink(recorder, req)
+
+		if recorder.Code != http.StatusCreated {
+			b.Errorf("handler returned wrong status code: got %v want %v", recorder.Code, http.StatusCreated)
+		}
+
+		contentType := recorder.Header().Get("Content-Type")
+		if contentType != "text/plain" {
+			b.Errorf("handler returned wrong content type: got %v want %v", contentType, "text/plain")
+		}
+	}
+}
+
+func BenchmarkMultiplePostNotFound(b *testing.B) {
+	os.Remove(config.Options.StorageFilePath)
+	for i := 0; i < b.N; i++ {
+		req := httptest.NewRequest(http.MethodGet, "/"+generateRandomString(128), nil)
+
+		recorder := httptest.NewRecorder()
+
+		handleGetLink(recorder, req)
+
+		if recorder.Code != http.StatusNotFound {
+			b.Errorf("handler returned wrong status code: got %v want %v", recorder.Code, http.StatusNotFound)
+		}
+	}
+}
+
+func generateRandomString(length int) string {
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(b)
 }
