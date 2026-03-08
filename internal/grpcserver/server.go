d4207758cd5a02a0d1a210469267e145a158656b
Vsevolod Ryzhov <v.ryzhov@plein.com>
2026-03-08 18:12:03 +0300

    iter28: wip gRPC

--- internal/grpcserver/server.go
+++ internal/grpcserver/server.go
@@ -3,9 +3,11 @@ package grpcserver
 
 import (
 	"context"
+	"errors"
 
 	"github.com/vsevolod-ryzhov/urlshortner.git/internal/logger"
 	"github.com/vsevolod-ryzhov/urlshortner.git/internal/service"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	pb "github.com/vsevolod-ryzhov/urlshortner.git/api/proto"
@@ -33,6 +35,7 @@ func (s *ShortenerServer) ShortenURL(ctx context.Context, in *pb.URLShortenReque
 	shortened, _, errCreation := service.CreateShortURL(ctx, in.GetUrl(), userID)
 	if errCreation != nil {
 		logger.Log.Debug("Shortened result was not saved to file", zap.Error(errCreation))
+		return &response, errCreation
 	}
 
 	response.SetResult(shortened)
@@ -43,11 +46,38 @@ func (s *ShortenerServer) ShortenURL(ctx context.Context, in *pb.URLShortenReque
 func (s *ShortenerServer) ExpandURL(ctx context.Context, in *pb.URLExpandRequest) (*pb.URLExpandResponse, error) {
 	var response pb.URLExpandResponse
 
+	url, err := service.GetURL(in.GetId())
+	if err != nil {
+		return &response, err
+	}
+
+	response.SetResult(url)
+
 	return &response, nil
 }
 
 func (s *ShortenerServer) ListUserURLs(ctx context.Context, _ *emptypb.Empty) (*pb.UserURLsResponse, error) {
 	var response pb.UserURLsResponse
+
+	userID, ok := service.GetUserIDFromContext(ctx)
+	if !ok {
+		return &response, errors.New("unauthorized")
+	}
+
+	urls, err := service.GetUserURLs(ctx, userID)
+	if err != nil {
+		return &response, errors.New("internal server error")
+	}
+
+	data := make([]*pb.URLData, len(urls))
+	for _, url := range urls {
+		ud := pb.URLData_builder{
+			ShortUrl:    proto.String(url.ShortURL),
+			OriginalUrl: proto.String(url.OriginalURL),
+		}.Build()
+		data = append(data, ud)
+	}
+	response.SetUrl(data)
 
 	return &response, nil
 }
